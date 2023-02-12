a2634ad8b7d0b55865aea128281bb80188c0b801
joseph-106 <sjjoseph106@gmail.com>
2023-02-12 16:31:07 +0900

    :tada: init

---
+++ android/app/src/main/kotlin/com/example/aram_gg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aram_gg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('ARAM GG'),
+        ),
+        body: const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
